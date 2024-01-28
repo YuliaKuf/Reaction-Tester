0e936c97d88ec25e4e768c6888fc5e553e21ff09
Yulia Kufman <yuliakufman28@gmail.com>
2024-01-28 17:52:34 +0200

    small changes in functions
    Changes in the  OnClick function.
    added the functions:
    changeShape()
    getRandomshape()
    getRandomColor()

--- reflexTester.js
+++ reflexTester.js
@@ -1,75 +1,74 @@
-var startTime = new Date().getTime();
+let startTime = new Date().getTime();
 let bestTime = 10000;
 let w = window.innerWidth;
 let h = window.innerHeight;
+let randomWidth = 50 + "px";
+let randomHeight = 50 + "px";
+let randomRadius = "50%";
+
+const shape = document.getElementById("changingShape");
+const timeCount = document.getElementById("timeCounter");
 
 //Functaion to make our random shape appear
-function makeShapeAppear(){
-    
-    document.getElementById("changingShape").style.display = "block";
-    startTime = new Date().getTime();
-
+function makeShapeAppear() {
+  shape.style.display = "block";
+  startTime = new Date().getTime();
 }
 
 function callToMakeShapeAppear() {
+  setTimeout(makeShapeAppear, Math.random() * 2000);
+}
 
-    setTimeout(makeShapeAppear,Math.random() * 2000);
+function getRandomshape() {
+  return Math.random() * 350 + "px";
+}
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
+function changeShape() {
+  shape.style.width = getRandomshape();
+  shape.style.height = getRandomshape();
+  shape.style.borderRadius = getRandomshape();
+  shape.style.backgroundColor = getRandomColor();
 }
 
 //First time the shape will appear
 callToMakeShapeAppear();
 
-//ON Shape Click 
-document.getElementById("changingShape").onclick = function() {
+//ON Shape Click
+shape.onclick = function () {
+  shape.style.display = "none";
 
+  let endTime = new Date().getTime();
+  let timeElapsed = (endTime - startTime) / 1000;
 
-    document.getElementById("changingShape").style.display = "none";
+  if (timeElapsed < bestTime) {
+    bestTime = timeElapsed;
 
-    let endTime = new Date().getTime();
-    let timeElapsed = (endTime - startTime)/1000;
+    alert("You Beat the best time! with a new record of: " + bestTime);
+  } else {
+    alert(
+      "Good luck beating the best score next time best score is curently: " +
+        bestTime +
+        " and your score is: " +
+        timeElapsed
+    );
+  }
 
-    if(timeElapsed < bestTime){
+  timeCount.innerHTML = "Time Count: " + timeElapsed;
 
-        bestTime = timeElapsed;
+  changeShape();
 
-        alert("You Beat the best time! with a new record of: " +bestTime);
-    } else {
+  //Changing the location
+  let randomTop =
+    Math.floor(Math.random() * (h - 400 - parseInt(randomHeight))) + "px";
+  let randomLeft =
+    Math.floor(Math.random() * (w - parseInt(randomWidth))) + "px";
+  shape.style.top = randomTop;
+  shape.style.left = randomLeft;
 
-        alert("Good luck beating the best score next time best score is curently: " + bestTime +" and your score is: " + timeElapsed); 
-    }
+  startTime = new Date().getTime();
 
-    document.getElementById("timeCounter").innerHTML = "Time Count: " + timeElapsed;
-
-    //Changing the shape - variables
-    let randomWidth = Math.floor(Math.random() * 500 ) + 10 + "px";
-    let randomHeight = Math.floor(Math.random() * 500 ) + 10 + "px";
-    let randomRadius = Math.floor(Math.random() * 100 ) + "px";
-
-    //Actually Changing the shape
-    this.style.width = randomWidth;
-    this.style.height = randomHeight;
-    this.style.borderRadius = randomRadius;
-
-    //Changing the color - variables
-    let randomColor = "#" + Math.floor(Math.random() * 16777215 ).toString(16);
-
-    //Actually Changing the color
-    this.style.backgroundColor = randomColor;
-
-    //Changing the location - variables
-  
-    let randomTop =  Math.floor(Math.random() * ((h-400) - parseInt(randomHeight))) + "px";
-    let randomLeft = Math.floor(Math.random() * ((w)- parseInt(randomWidth))) + "px";
-
-    //Actually Changing the location
-    this.style.top = randomTop;
-    this.style.left  = randomLeft;
-
-    startTime = new Date().getTime();
-
-    callToMakeShapeAppear();
-
-}
-
-
+  callToMakeShapeAppear();
+};
